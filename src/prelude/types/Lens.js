5659c5cbb7bda7fa5332cf09d8cd980fae9cf48a
Marina Miyaoka <miyaokamarina@gmail.com>
2018-03-18 03:53:38 +0300

    feat: Added dual functors implementation to `Lens`

--- src/prelude/types/Lens.js
+++ src/prelude/types/Lens.js
@@ -2,10 +2,15 @@ import { compose } from 'prelude/compose';
 import { type } from 'prelude/type';
 
 import {
+  $IsCocontravariant,
+  $IsCofunctor,
+  $IsCoprofunctor,
   $IsGetter,
   $IsSemigroupoid,
   $IsSetter,
 } from 'prelude/_symbols';
+
+// TODO: Prop, path, head, last, id.
 
 @type
 export class Lens {
@@ -14,10 +19,16 @@ export class Lens {
     this.s = setter;
   }
 
+  cocontramap = (f, g) => type(Lens)(compose(this.g, f), (v, x) => g(this.s(v, f(x))));
+  comap = (f, g) => type(Lens)(compose(g, this.g), (v, x) => this.s(f(v), x));
   compose = l => type(Lens)(compose(l.g, this.g), (v, x) => this.s(l.s(v, this.g(x)), x));
+  copromap = (f, g, f$, g$) => this.cocontramap(f, g).comap(f$, g$);
   get = x => this.g(x);
   set = (v, x) => this.s(v, x);
 
+  [$IsCocontravariant] = true;
+  [$IsCofunctor] = true;
+  [$IsCoprofunctor] = true;
   [$IsGetter] = true;
   [$IsSemigroupoid] = true;
   [$IsSetter] = true;
